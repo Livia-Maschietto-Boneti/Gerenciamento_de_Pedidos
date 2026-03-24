9bb6fc12bb5c6753dff1a45b91bc15644474a413
Liv <edwardghostiie@gmail.com>
2026-03-24 19:46:18 -0300

    primeiro

---
+++ src/main/java/liv/duda/DudaApplication.java
@@ -0,0 +1,13 @@
+package liv.duda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DudaApplication.class, args);
+	}
+
+}
